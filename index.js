bf03084993fe2313fa230f43bd6c258cafcd970f
Nicodemos-Dev <nicodemoszenza.dev@gmail.com>
2024-07-15 02:00:02 +0100

    Desafio: Classificador de nível de Herói

---
+++ index.js
@@ -0,0 +1,69 @@
+// Se XP for menor do que 1.000 ---> Ferro
+let nomeDoHeroi = "Ferro"
+let valorXP = 999
+
+if(valorXP < 1000) {
+    console.log("O Herói de nome " + nomeDoHeroi + " " + "está no nível de " + valorXP)
+}
+
+// Se XP for entre 1.001 e 2.000 ---> Bronze
+let nomeDoHeroi2 = "Bronze"
+let valorXP2 = 1999
+
+if((valorXP2 > 1001) && (valorXP2 < 2000)) {
+    console.log("O Herói de nome " + nomeDoHeroi2 + " " + "está no nível de " + valorXP2)
+}
+
+
+// Se XP for entre 2.001 e 5.000 ---> Prata
+let nomeDoHeroi3 = "Prata"
+let valorXP3 = 4999
+
+if((valorXP3 > 2001) && (valorXP3 < 5000)) {
+    console.log("O Herói de nome " + nomeDoHeroi3 + " " + "está no nível de " + valorXP3)
+}
+
+
+// Se XP for entre 5.001 e 7.000 ---> Ouro
+let nomeDoHeroi4 = "Ouro"
+let valorXP4 = 6999
+
+if((valorXP4 > 5001) && (valorXP4 < 7000)) {
+    console.log("O Herói de nome " + nomeDoHeroi4 + " " + "está no nível de " + valorXP4)
+}
+
+
+// Se XP for entre 7.001 e 8.000 ---> Platina
+let nomeDoHeroi5 = "Platina"
+let valorXP5 = 7999
+
+if((valorXP5 > 7001) && (valorXP5 < 8000)) {
+    console.log("O Herói de nome " + nomeDoHeroi5 + " " + "está no nível de " + valorXP5)
+}
+
+
+// Se XP for entre 8.001 e 9.000 ---> Ascendente
+let nomeDoHeroi6 = "Ascendente"
+let valorXP6 = 8999
+
+if((valorXP6 > 8001) && (valorXP6 < 9000)) {
+    console.log("O Herói de nome " + nomeDoHeroi6 + " " + "está no nível de " + valorXP6)
+}
+
+
+// Se XP for entre 9.001 e 10.000 ---> Imortal
+let nomeDoHeroi7 = "Imortal"
+let valorXP7 = 9999
+
+if((valorXP7 > 9001) && (valorXP7 < 10000)) {
+    console.log("O Herói de nome " + nomeDoHeroi7 + " " + "está no nível de " + valorXP7)
+}
+
+
+// Se XP for maior ou igual a 10.001 ---> Radiante
+let nomeDoHeroi8 = "Radiante"
+let valorXP8 = 10002
+
+if(valorXP8 >= 10001) {
+    console.log("O Herói de nome " + nomeDoHeroi8 + " " + "está no nível de " + valorXP8)
+}
